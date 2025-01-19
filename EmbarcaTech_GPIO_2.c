2343e3f06ff8773efa9ac23c20853597c3f96264
DIEGO MICKAEL <diego.bme@hotmail.com>
2025-01-19 02:02:30 -0300

    Implantado menu, função interação com led verde, biblioteca pico/bootrom.h, identação, função BOOTSEL

--- EmbarcaTech_GPIO_2.c
+++ EmbarcaTech_GPIO_2.c
@@ -1,30 +1,37 @@
 // Bibliotecas que serão usadas no projeto
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
 
-#define LED_RED_PIN 13 // Pino para o led vermelho
+#define LED_RED_PIN 13   // Pino para o led vermelho
 #define LED_GREEN_PIN 11 // Ino para o led verde
-#define LED_BLUE_PIN 12 // Pino para o led azul
-#define BUZZER_PIN 21 // Pino para o buzzer
+#define LED_BLUE_PIN 12  // Pino para o led azul
+#define BUZZER_PIN 21    // Pino para o buzzer
 
 // Definição das funções que serão usadas no projeto
 void initialize_gpio();
 void control_components(char command);
 void buzzer_beep();
+void menu();
 
 // Função principal
-int main() {
+int main()
+{
     // Inicializa o UART e os GPIOs
     stdio_init_all();
     initialize_gpio();
-
+    getchar();
     printf("Sistema iniciado. Aguardando comandos...\n");
+    menu();
 
     // Loop para pegar os comandos do terminal
-    while(1) {
+    while (1)
+    {
+        
         char command; // Guarda o comando vindo do teminal
 
-        if (scanf("%c", &command)) {
+        if (scanf("%c", &command))
+        {
             control_components(command); // Faz a palaca executar uma açao
         }
     }
@@ -32,7 +39,8 @@ int main() {
 } // Fim main
 
 // Função para inicializar os pinos GPIO
-void initialize_gpio() {
+void initialize_gpio()
+{
     // Pinos para o led vermelho
     gpio_init(LED_RED_PIN);
     gpio_set_dir(LED_RED_PIN, GPIO_OUT);
@@ -51,36 +59,58 @@ void initialize_gpio() {
 } // Fim initialize_gpio
 
 // Função para controlar os componentes da placa usando comandos do terminal
-void control_components(char command) {
-    switch(command) {
-        case '1':
-            break;
-        case '2':
-            break;
-        case '3':
-            break;
-        case '4':
-            break;
-        case '5':
-            break;
-        case '6':
-            // Comando para o buzzer fazer barulho
-            buzzer_beep();
-            break;
-        default:
-            printf("Comando inválido!\n");
-            break;
+void control_components(char command)
+{
+    switch (command)
+    {
+    case '1':
+        gpio_put(LED_GREEN_PIN,1);
+        printf("Luz verde acesa!\n");
+        break;
+    case '2':
+        break;
+    case '3':
+        break;
+    case '4':
+        break;
+    case '5':
+        break;
+    case '6':
+        // Comando para o buzzer fazer barulho
+        buzzer_beep();
+        break;
+    case '7':
+        printf("Entrando no modo BOOTSEL...");
+        sleep_ms(1000);
+        reset_usb_boot(0,0);
+        break;
+    default:
+        printf("Comando inválido!\n");
+        break;
     }
 }
 
 // Função para fazer o buzzer emitir um barulho
-void buzzer_beep() {
+void buzzer_beep()
+{
     int contB;
 
-    for(contB = 0; contB < 10000; contB++) {
+    for (contB = 0; contB < 10000; contB++)
+    {
         gpio_put(BUZZER_PIN, 1);
         sleep_us(50);
         gpio_put(BUZZER_PIN, 0);
         sleep_us(50);
     }
 } // Fim buzzer_beep
+
+void menu(){
+    printf("Entre com a opcao desejada: \n");
+    printf("[1] Liga LED verde;\n");
+    printf("[2] Liga LED azul;\n");
+    printf("[3] Liga LED vermelho;\n");
+    printf("[4] Liga os três LEDs;\n");
+    printf("[5] Desliga todos os LEDs;\n");
+    printf("[6] Aciona o Buzzer por 2 segundos;\n");
+    printf("[7] Sai do modo execucao e entra no modo BOOTSEL\n");
+}
